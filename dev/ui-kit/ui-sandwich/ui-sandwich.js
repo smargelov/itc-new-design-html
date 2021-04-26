2ef97613d24ab8b69aa98dae762a034f05295604
Sergey Margelov <smargelov@gmail.com>
2021-04-26 15:28:08 +0300

    Fix big title

--- dev/ui-kit/ui-sandwich/ui-sandwich.js
+++ dev/ui-kit/ui-sandwich/ui-sandwich.js
@@ -1,11 +1,8 @@
-const sandwichToggle = function () {
-    // Выбираем элементы, которые нам нужны. В примере мы ищем элементы с классом "sandwich"
-    let sandwichElements = document.querySelectorAll('.sandwich');
-    // Проходим циклом по всем эдементам и на каждый элемент вешаем слушателя, который по клику будет переключать класс
-    sandwichElements.forEach((item) => {
+const sandwichToggle = () => {
+    const sandwichElements = document.querySelectorAll('.sandwich');
+    sandwichElements.forEach(item => {
         item.addEventListener('click', showSandwichTarget);
     });
-
     function showSandwichTarget() {
         let targetId = this.getAttribute('data-target-id'),
             targetClassToggle = this.getAttribute('data-target-class-toggle');
